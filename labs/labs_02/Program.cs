5c9adbffb0d92bb11cad33428f964e52f5a2849d
Ehiuwa Oronsaye <EOronsaye@spartaglobal.com>
2019-06-24 10:26:35 +0100

    Initial commit Visual Studio

---
+++ labs/labs_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From Theresa!!");
+            Console.ReadLine();
+        }
+    }
+}
