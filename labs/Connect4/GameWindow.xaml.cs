14b4cf4a780a97a0d691f71a5d8d01cde8ca01e7
Ehiuwa Oronsaye <EOronsaye@spartaglobal.com>
2019-07-23 13:30:33 +0100

    Fill Column One

--- labs/Connect4/GameWindow.xaml.cs
+++ labs/Connect4/GameWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,20 +22,113 @@ namespace Connect4
     /// </summary>
     public partial class GameWindow : MetroWindow
     {
+
         public GameWindow()
         {
             InitializeComponent();
             Initialise();
-            //PlayerOne.Content =
         }
+
+        private void SwapPlayer(int playerNumber)
+        {
+            /*if (playerNumber == 1)
+            {
+                Players.Fill = Brushes.BlanchedAlmond;
+            }
+            else if (playerNumber == 2)
+            {
+                Players.Fill = Brushes.LightGray;
+            }*/
+        }
+
+        
 
         void Initialise()
         {
-
+           // SwapPlayer(1);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        static int counter = 0;
+        private bool turn = true;
+        private void C1_Click(object sender, RoutedEventArgs e)
         {
+            Button c1 = (Button)sender;
+            
+
+            if (counter == 0)
+            {
+                if (turn)
+                {
+                    C1P1.Fill = Brushes.BlanchedAlmond;
+                }
+                else
+                {
+                    C1P1.Fill = Brushes.LightGray;
+                }
+                turn = !turn;
+            }
+            if (counter == 1)
+            {
+                if (turn)
+                {
+                    C1P2.Fill = Brushes.BlanchedAlmond;
+                }
+                else
+                {
+                    C1P2.Fill = Brushes.LightGray;
+                }
+                turn = !turn;
+            }
+            if (counter == 2)
+            {
+                if (turn)
+                {
+                    C1P3.Fill = Brushes.BlanchedAlmond;
+                }
+                else
+                {
+                    C1P3.Fill = Brushes.LightGray;
+                }
+                turn = !turn;
+            }
+            if (counter == 3)
+            {
+                if (turn)
+                {
+                    C1P4.Fill = Brushes.BlanchedAlmond;
+                }
+                else
+                {
+                    C1P4.Fill = Brushes.LightGray;
+                }
+                turn = !turn;
+            }
+            if (counter == 4)
+            {
+                if (turn)
+                {
+                    C1P5.Fill = Brushes.BlanchedAlmond;
+                }
+                else
+                {
+                    C1P5.Fill = Brushes.LightGray;
+                }
+                turn = !turn;
+            }
+            if (counter == 5)
+            {
+                if (turn)
+                {
+                    C1P6.Fill = Brushes.BlanchedAlmond;
+                }
+                else
+                {
+                    C1P6.Fill = Brushes.LightGray;
+                }
+                turn = !turn;
+            }
+            counter++;
 
         }
     }
