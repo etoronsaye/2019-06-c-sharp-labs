cbaa83ed91a01037b1eb0be735fc9dc455cb697b
Ehiuwa Oronsaye <EOronsaye@spartaglobal.com>
2019-07-24 09:30:33 +0100

    Connect 4 multiple versions

---
+++ labs/Connect4V2/Chips.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4V2
+{
+    public enum Chips
+    {
+        Empty,
+        Burlywood,
+        Gray
+    }
+}
---
+++ labs/Connect4V2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Connect4V2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string playerOne = "";
+        string playerTwo = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            playerOne = P1.Text;
+            playerTwo = P2.Text;
+
+            GameWindow gameWindow = new GameWindow();
+            this.Close();
+            gameWindow.Show();
+        }
+    }
+}
